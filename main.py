2c2077ab446e26abef7e382ee4ef21e63431faae
chisoftltd <chisoftltd@gmail.com>
2021-12-27 15:16:23 +0000

    Setting up the game environment

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ from ball import Ball
 from score import Score
 import time
 
-# Set screen environment
+#   Set screen environment
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.title("PongGame")
 screen.tracer(0)
 
-# Declare objects for left and right paddles
+#   Declare objects for left and right paddles
 r_paddle = Paddle(350, 0)
 l_paddle = Paddle(-350, 0)
 ball = Ball()
 score = Score()
 
-#Setup key listen
+#   Setup key listen
 screen.listen()
 screen.onkey(r_paddle.go_up, "Up")
 screen.onkey(r_paddle.go_down, "Down")
@@ -26,3 +26,25 @@ screen.onkey(l_paddle.gp_down, "s")
 
 
 game_is_on = True
+while game_is_on:
+    screen.update()
+    ball.move()
+
+    #   Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_x()
+
+    #   Detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    #   Detect R paddle misses
+    if ball.xcor() > 360:
+        ball.reset_position()
+        score.l_point()
+
+    #   Detect L paddle misses
+    if ball.xcor() < -360:
+        ball.reset_position()
+        score.r_point()
+screen.exitonclick()
